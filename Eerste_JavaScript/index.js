c470aab9a774975f4c1ddceccf70ea83d5362c80
melissadehaan91 <80722664+melissadehaan91@users.noreply.github.com>
2021-04-23 19:37:17 +0200

    Add files via upload

---
+++ Eerste_JavaScript/index.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy")
+let name = 'Melissa';
+console.log(name);
+console.log(4+4);
+console.log('4'+'4');
+console.log(4*5);
+console.log(5%3);
+let age = 29;
+console.log(typeof age);
+age = '29';
+console.log(typeof age);
